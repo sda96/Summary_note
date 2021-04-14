37520d41c0f162568b4d6631e81cc65780809a2d
sda96 <sda96@naver.com>
2021-04-14 23:56:52 +0900

    백준 수학문제

--- Coding_Test/baekjoon/연습장.py
+++ Coding_Test/baekjoon/연습장.py
@@ -1,4 +1,9 @@
-import sys
-for line in sys.stdin.readlines():
-    a, b = map(int, line.split(" "))
-    print(a + b)
+from sys import stdin
+from collections import deque
+input = stdin.readline
+
+n, m = map(int, input().split())
+a = [list(input().strip()) for _ in range(n)]
+check = [[[[False]*m for _ in range(n)] for _ in range(m)] for _ in range(n)]
+dx, dy = (-1, 0, 1, 0), (0, 1, 0, -1)
+q = deque()
---
+++ Coding_Test/programmers/연습장.py
@@ -0,0 +1,4 @@
+S = "baabaa"
+for i in range(len(S), 2):
+    print(S)
+    
